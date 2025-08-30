a57bb19718ee4cec1bf780e8fb231a81cab2c41b
unknown <sumitdiwakar476@gmai.com>
2025-08-30 21:09:04 +0530

    deploy sucessfull

--- server/index.js
+++ server/index.js
@@ -15,7 +15,9 @@ connectDB();
 const app = express();
 
 // Middleware setup
-app.use(cors()); // Use cors
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173"
+})); // Use cors
 app.use(express.json()); // Use express.json to parse JSON bodies
 
 const PORT = process.env.PORT || 5000;
