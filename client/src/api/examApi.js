207a0e1a8099fcb89d51eced6ded787106643fbe
unknown <sumitdiwakar476@gmai.com>
2025-08-30 21:02:14 +0530

    deploy sucessfull

--- client/src/api/examApi.js
+++ client/src/api/examApi.js
@@ -1,7 +1,7 @@
 // src/api/examApi.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; // Your backend URL
+const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'; // Your backend URL
 
 export const fetchQuestions = async (className, subject) => {
   try {
